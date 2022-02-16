cf0a87fbe5e4cfae8183795828c05d50b11d188d
leidypaez <leidy16paez@gmail.com>
2022-02-16 02:30:00 -0500

    subiendo el router de cada componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LandingPageComponent},
+  {path: "signIn", component:LoginComponent},
+  {path: "signUp", component:RegisterComponent},
+  {path: "**", redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
